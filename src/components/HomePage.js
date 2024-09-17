5f4e51f454362171bfef410c296e684ab80837bc
maxwellmuchiri <muchirimaxwel@gmail.com>
2024-09-18 01:01:00 +0300

    caurosel sliding

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,3 @@
-// components/HomePage.js
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './HomePage.css'; 
@@ -15,13 +14,13 @@ const HomePage = () => {
         <div id="carouselExample" className="carousel slide" data-bs-ride="carousel">
           <div className="carousel-inner">
             <div className="carousel-item active">
-              <img src="/path-to-signage1.jpg" className="d-block w-100" alt="Signage Type 1" />
+              <img src="https://scontent-mba1-1.xx.fbcdn.net/v/t39.30808-6/376620967_624687836452328_4126194712302784690_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=127cfc&_nc_ohc=y4-gB2JNUmcQ7kNvgGYMrRk&_nc_ht=scontent-mba1-1.xx&_nc_gid=AsU5-2bKSnnub2_mFNK-W7j&oh=00_AYDIGl1Oe1vWozAGThmsEeCyapXFLVLmau9UJn8WjBB_Yg&oe=66EFC36F" className="d-block w-100" alt="Signage Type 1" />
             </div>
             <div className="carousel-item">
-              <img src="/path-to-signage2.jpg" className="d-block w-100" alt="Signage Type 2" />
+              <img src="https://scontent-mba1-1.xx.fbcdn.net/v/t39.30808-6/376646800_624687526452359_9100086174277594774_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=127cfc&_nc_ohc=GJnu7-pGGvIQ7kNvgFbG5Ci&_nc_ht=scontent-mba1-1.xx&_nc_gid=ASpf5XxtqWENN3QEVa8sax9&oh=00_AYARz8G88Bbw4A9m0t396TWJBC-ZQuQjLPTBKz8IkW6Lng&oe=66EFBC5D" className="d-block w-100" alt="Signage Type 2" />
             </div>
             <div className="carousel-item">
-              <img src="/path-to-signage3.jpg" className="d-block w-100" alt="Signage Type 3" />
+              <img src="https://scontent-mba1-1.xx.fbcdn.net/v/t39.30808-6/376621405_624687686452343_7779565930076370935_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=127cfc&_nc_ohc=wf2gsC76qg0Q7kNvgH5z-pO&_nc_ht=scontent-mba1-1.xx&oh=00_AYDOLa7_hclfDKlEMrI-aMI1rhqJMwnzv3a7LrmJkmCp2g&oe=66EFB7BD" className="d-block w-100" alt="Signage Type 3" />
             </div>
           </div>
           <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
